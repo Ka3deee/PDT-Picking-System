c904ea4a860f1c30ea4a17a194bac7cdc7eb4c87
Kirk <kirkowenmendonez.jesalva@bicol-u.edu.ph>
2025-12-02 07:58:34 +0800

    Updated Start Picking

---
+++ PDTPickingSystem/Helpers/EntryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace PDTPickingSystem.Helpers
+{
+    public static class EntryExtensions
+    {
+        // Attached property to mimic WinForms Tag
+        public static readonly BindableProperty TagProperty =
+            BindableProperty.CreateAttached(
+                "Tag",               // Name
+                typeof(object),      // Type
+                typeof(EntryExtensions), // Owner type
+                null);               // Default value
+
+        // Getter
+        public static object GetTag(BindableObject view)
+        {
+            return view.GetValue(TagProperty);
+        }
+
+        // Setter
+        public static void SetTag(BindableObject view, object value)
+        {
+            view.SetValue(TagProperty, value);
+        }
+    }
+}
