e91b44911a6158a0478f0e003016c73af7cb1c55
Kirk <kirkowenmendonez.jesalva@bicol-u.edu.ph>
2026-01-12 15:23:53 +0800

    Updated MauiProgram to enable creating a file on Android data folder

--- PDTPickingSystem/MauiProgram.cs
+++ PDTPickingSystem/MauiProgram.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
 using PDTPickingSystem.Helpers.Interfaces;
 using Plugin.Maui.Audio;
+
 #if ANDROID
+using Android.App;
 using PDTPickingSystem.Platforms.Android;
+using System.IO;
 #endif
 
 namespace PDTPickingSystem
@@ -12,6 +15,7 @@ namespace PDTPickingSystem
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
@@ -24,26 +28,60 @@ namespace PDTPickingSystem
 #if ANDROID
             builder.Services.AddSingleton<IWifiService, WifiService_Android>();
 #else
-            builder.Services.AddSingleton<IWifiService, PDTPickingSystem.Helpers.WifiService_Default>();
+            builder.Services.AddSingleton<IWifiService, WifiService_Default>();
 #endif
 
             // Register Audio Service for Idle Alarm
             builder.Services.AddSingleton(AudioManager.Current);
 
             // REMOVE ENTRY UNDERLINE (Android)
-            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
-            {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(
+                "NoUnderline",
+                (handler, view) =>
+                {
 #if ANDROID
-                handler.PlatformView.BackgroundTintList =
-                    Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+                    handler.PlatformView.BackgroundTintList =
+                        Android.Content.Res.ColorStateList.ValueOf(
+                            Android.Graphics.Color.Transparent);
 #endif
-            });
+                });
 
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
+            var app = builder.Build();
+
+#if ANDROID
+            // Delete old root-level wifi.txt if it exists
+            _ = DeleteOldRootWifiFileAsync();
+#endif
+
+            return app;
         }
+
+#if ANDROID
+        private static async Task DeleteOldRootWifiFileAsync()
+        {
+            try
+            {
+                var dir = Android.App.Application.Context.GetExternalFilesDir(null);
+                if (dir == null)
+                    return;
+
+                string oldWifiFile = Path.Combine(dir.AbsolutePath, "wifi.txt");
+                if (File.Exists(oldWifiFile))
+                    File.Delete(oldWifiFile);
+
+                // No need to create a new wifi.txt here; AppGlobal handles it in Backup/PDTPicking
+            }
+            catch (System.Exception ex)
+            {
+                Android.Util.Log.Warn(
+                    "PDTPickingSystem",
+                    $"Failed to delete old wifi.txt: {ex.Message}");
+            }
+        }
+#endif
     }
 }
